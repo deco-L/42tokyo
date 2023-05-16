b11a151c7a8eecc03102a61b93cdde4bff7d3050
Chiaki Sakamoto <csakamot@c1r25s1.42tokyo.jp>
2023-05-16 21:04:19 +0900

    V V

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 20:53:02 by csakamot          #+#    #+#             */
-/*   Updated: 2023/05/16 20:54:12 by csakamot         ###   ########.fr       */
+/*   Updated: 2023/05/16 21:02:50 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,15 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
+	unsigned int	i;
 
+	i = 0;
+	if (!s || !f)
+		return ;
+	while (s[i] != '\0')
+	{
+		f(i, &s[i]);
+		i++;
+	}
+	return ;
 }
