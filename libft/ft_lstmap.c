51c6dbe97ab15348e8a45262ca3b7224bb766d2f
deco-L <scotchwitchbroom@gmail.com>
2023-05-30 16:50:40 +0900

    V V

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 16:07:55 by csakamot          #+#    #+#             */
-/*   Updated: 2023/05/30 16:15:36 by csakamot         ###   ########.fr       */
+/*   Updated: 2023/05/30 16:50:29 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,27 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*tmp;
 	t_list	*new;
 
-	if (lst && f && del)
+	if (!lst || !f || !del)
+		return (NULL);
+	new = ft_lstnew(f(lst -> content));
+	if (!new)
 	{
-		new = (t_list *)malloc(sizeof(t_list));
-		if (new == NULL)
+		ft_lstdelone(lst, del);
+		return (NULL);
+	}
+	while (lst -> next)
+	{
+		tmp = ft_lstnew(f(lst -> content));
+		if (!tmp)
+		{
+			ft_lstdelone(tmp, del);
 			return (NULL);
-		
+		}
+		ft_lstadd_back(&new, tmp);
+		lst = lst -> next;
 	}
 	return (new);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 09:01:05 by csakamot          #+#    #+#             */
-/*   Updated: 2023/05/30 16:05:07 by csakamot         ###   ########.fr       */
+/*   Updated: 2023/05/30 16:39:05 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
 void	*ft_memchr(const void *buf, int c, size_t n);
