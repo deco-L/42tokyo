09cbf8ff606706f2e63d32f1d9d7c0e2ddf784df
deco-L <scotchwitchbroom@gmail.com>
2023-06-02 20:54:24 +0900

    V V

--- printf/test.c
+++ printf/test.c
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 14:32:37 by csakamot          #+#    #+#             */
-/*   Updated: 2023/06/02 20:34:06 by csakamot         ###   ########.fr       */
+/*   Updated: 2023/06/02 20:53:46 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,8 @@ int	main(void)
 	// printf("%u\n", 42967295);
 	// printf("%llu\n", d);
 	// printf("%X\n%p\n", 1234, c);
-	ft_printf("aaa|%i|aaa%%ccc%Xbbb  |%p|\n", 123, 1234, c);
+	ft_printf("aaa|%i|aaa%%ccc%Xbbb  |\n", 123, 1234);
 	ft_printf("%d\n", ft_printf(":aaa:"));
+	free(c);
 	return (0);
 }
