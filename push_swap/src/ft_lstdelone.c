4ca5142cfacdcc54cc7684fb56e45631317c0390
deco-L <scotchwitchbroom@gmail.com>
2023-06-15 17:06:59 +0900

    V V

---
+++ push_swap/src/ft_lstdelone.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/30 14:14:24 by csakamot          #+#    #+#             */
+/*   Updated: 2023/05/30 14:39:44 by csakamot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	if (lst && del)
+	{
+		del(lst -> content);
+		free (lst);
+	}
+}
