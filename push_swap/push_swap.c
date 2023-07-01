ae50b1e08a44c20f41821ddc7e5ad8a1a76e827d
Chiaki Sakamoto <csakamot@c1r26s7.42tokyo.jp>
2023-07-01 19:46:46 +0900

    V V

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 17:09:40 by csakamot          #+#    #+#             */
-/*   Updated: 2023/07/01 18:32:36 by csakamot         ###   ########.fr       */
+/*   Updated: 2023/07/01 19:46:22 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,10 @@ int	main(int argc, char **argv)
 	return (0);
 }
 
-// #include <libc.h>
+#include <libc.h>
 
-// __attribute__((destructor))
-// static void destructor() {
-//     system("leaks -q push_swap");
-// }
+__attribute__((destructor))
+static void destructor() {
+    system("leaks -q push_swap");
+}
 // compaction of coordinates
