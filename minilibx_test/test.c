dd5972a2d475fd6686005bc88110d0c270073727
Chiaki Sakamoto <csakamot@c1r26s7.42tokyo.jp>
2023-07-01 19:10:18 +0900

    V V

--- minilibx_test/test.c
+++ minilibx_test/test.c
@@ -6,7 +6,18 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:20:46 by csakamot          #+#    #+#             */
-/*   Updated: 2023/07/01 14:20:48 by csakamot         ###   ########.fr       */
+/*   Updated: 2023/07/01 19:00:01 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <mlx.h>
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1000, "Hello world!");
+	mlx_loop(mlx);
+}
